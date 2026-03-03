e59f52abb30bd2898a71c52c726bb6b661aaf28e
freudyjungebooks-source <freudyjungebooks@gmail.com>
2026-03-03 18:07:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import SymbolSearch from "./components/SymbolSearch";
+import SymbolGraph from "./components/SymbolGraph";
 
 function App() {
-  return <SymbolSearch />;
+  return (
+    <div style={{ padding: 20 }}>
+      <SymbolSearch />
+      <hr style={{ margin: "40px 0" }} />
+      <h2>Mapa Simbólico Interactivo</h2>
+      <SymbolGraph />
+    </div>
+  );
 }
 
 export default App;
